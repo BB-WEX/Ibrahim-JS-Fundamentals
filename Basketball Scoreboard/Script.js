5b4cf1fa44b3866274b109983a8f8aaaa4d96797
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-07-08 10:17:52 +0100

    Added highlight bigger score

--- Basketball Scoreboard/Script.js
+++ Basketball Scoreboard/Script.js
@@ -7,6 +7,14 @@ function AddScore(Score, Target){
     Score = Number(Score);
     Target.innerText = Number(Target.innerText) + Score;
     Target.innerText = (Number(Target.innerText)).toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping:false});
+
+    if(Target.innerText > Home.innerText){
+        Away.style.color = "gold";
+        Home.style.color = "white";
+    }else if(Target.innerText > Away.innerText){
+        Away.style.color = "white";
+        Home.style.color = "gold";
+    }
 }
 
 function Reset(Target){
