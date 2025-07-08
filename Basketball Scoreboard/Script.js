a2533d33212ecf4182565ce41ccd3b5c9d62f546
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-07-08 09:56:58 +0100

    Added Reset button

--- Basketball Scoreboard/Script.js
+++ Basketball Scoreboard/Script.js
@@ -4,7 +4,12 @@ const Away = document.getElementById("AWAYcounter");
 
 
 function AddScore(Score, Target){
-    console.log(Target);
     Score = Number(Score);
-    Target.innerText = Number(Target.innerText) + Score;    
+    Target.innerText = Number(Target.innerText) + Score;
+    Target.innerText = (Number(Target.innerText)).toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping:false});
+}
+
+function Reset(Target){
+    Target.innerText = 0;
+    Target.innerText = (Number(Target.innerText)).toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping:false});
 }
